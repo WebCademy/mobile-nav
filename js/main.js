ac4a1a6fc1f932e84be0b0761ae2bb26b9f6a144
Yurij <yurij.kharin@gmail.com>
2019-10-10 14:04:15 +0300

    FC

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('#menu-togle');
+const mobileNavContainer = document.querySelector('#mobile-nav');
+
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-nav--active');
+}
